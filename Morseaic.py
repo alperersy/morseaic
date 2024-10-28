94c9e4c41d6b12da52e91235c0d0a0f6d0b41ea4
yefesaktas <yusufefe29aktas@gmail.com>
2024-10-28 16:12:18 +0300

    pattern_transformer function and .gitignore file created, tested with SOS signal, successful result achieved.

--- Morseaic.py
+++ Morseaic.py
@@ -77,17 +77,19 @@ def stop_recording():
             print("****** Stopping the recording... ******\n\n")
             recording_flag=False
 
-def pattern_recognition(normalized_audio_data,SAMPLE_RATE,chunk_duration=0.01):
+def pattern_recognition(normalized_audio_data,SAMPLE_RATE, chunk_duration=0.01):
 
     # Calculate the number of samples per chunk
     chunk_samples = int(chunk_duration * SAMPLE_RATE)
 
     # Initialize the start and end indices for the chunks
     start_idx = 0
-    pattern_counter=0
-    space_counter=0
+    pattern_counter = 0
+    space_counter = 0
+
     global firstsignalflag
-    firstsignalflag=False
+    firstsignalflag = False
+
     # Array to store the pattern of each chunk
     pattern_array = []
 
@@ -102,33 +104,64 @@ def pattern_recognition(normalized_audio_data,SAMPLE_RATE,chunk_duration=0.01):
         # Calculate the mean amplitude of the chunk
         mean_amplitude = np.mean(np.abs(chunk))
 
-        # Convert mean amplitude to dB using librosa
+        # Convert mean amplitude to dB
         mean_amplitude_db = 20 * np.log10(mean_amplitude)
         mean_amplitude_db = round(mean_amplitude_db,2)
         
 
-        if mean_amplitude_db >-22:
-             pattern_counter+=1
-             if space_counter>15 and space_counter<=45:
+        if mean_amplitude_db > -20:
+             pattern_counter += 1
+
+             if space_counter > 10 and space_counter <= 50 and firstsignalflag == True:
                  pattern_array.append("type1")
-             if space_counter>45 and space_counter<90:
+
+             if space_counter > 50 and firstsignalflag == True:
                  pattern_array.append("type2")
-             firstsignalflag=True
-             space_counter=0
-        elif firstsignalflag==True and mean_amplitude_db<=-20:
-             space_counter+=1
-             if pattern_counter>3 and pattern_counter<=50:
+
+             space_counter = 0
+
+        elif mean_amplitude_db <= -20:
+             space_counter += 1
+
+             if pattern_counter > 10 and pattern_counter <= 55:
                  pattern_array.append('.')
-             elif pattern_counter>50 and pattern_counter<90:
+                 firstsignalflag = True
+
+             elif pattern_counter > 50:
+                 firstsignalflag = True 
                  pattern_array.append('-')
              
-             pattern_counter=0
+             pattern_counter = 0
 
         # Update the start index for the next chunk
         start_idx += chunk_samples
 
     return pattern_array
 
+def pattern_transformer(pattern_array, int_morse_table):
+    temp_element_list = []
+    temp_letter_list = []
+    output_string = []
+
+    for i in range(len(pattern_array)):
+        element = pattern_array[i]
+
+        if element == "type2":
+            temp_letter_list.append("".join(temp_element_list))
+
+            output_string.append(int_morse_table["".join(temp_letter_list)])
+            output_string.append(" ")
+            
+            temp_element_list.clear()
+            temp_letter_list.clear()
+
+        elif element != "type1" and element != "type2":
+            temp_element_list.append(element)
+
+
+    translated_string = "".join(output_string)
+
+    return translated_string
 
 def main():
     global normalized_audio_data, SAMPLE_RATE
@@ -210,10 +243,17 @@ def main():
              continue
         else:
              break
+        
+    # Pattern recognition process
+    pattern_array = pattern_recognition(normalized_audio_data, SAMPLE_RATE) 
+    
+    # TEST LINES DEL WHEN DONE
+    print(pattern_array) #print the pattern array
+    # TEST LINES END
 
-
+    translated_string = pattern_transformer(pattern_array, int_morse_table)
+    print(translated_string)
+    
 if __name__ == "__main__":
     main() # Run the main function
 
-pattern_array=pattern_recognition(normalized_audio_data,SAMPLE_RATE) #pattern recognition function
-print(pattern_array) #print the pattern array
